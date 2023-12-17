e7aae2889168031a98360d2e1f90c9e32e3550c1
mkg1 <gehring.mg@gmail.com>
2023-12-17 12:12:16 -0500

    Update logic to fix breaking bug

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import Questions from '../questions.js';
+import Complete from '../assets/quiz-complete.png';
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length; //deriving active question index rather than managing it as state is better practice (less state to manage)
 
-    const shuffledAnswers = [...Questions[activeQuestionIndex].answers]; //create new array to enure og array stays as is (unshuffled)
-    shuffledAnswers.sort((a, b) => Math.random() - 0.5 );
+    const quizIsComplete = activeQuestionIndex === Questions.length;
 
     function handleSelectAnswers(selectedAnswer) {
         setUserAnswers((prevUserAnswers) => {
@@ -15,7 +15,15 @@ export default function Quiz() {
         });
     }
 
+    if (quizIsComplete) {
+        return <div id="summary">
+            <img src={Complete} alt="trophy icon" />
+            <h2>Quiz Completed</h2>
+        </div>
+    }
 
+    const shuffledAnswers = [...Questions[activeQuestionIndex].answers]; //create new array to enure og array stays as is (unshuffled)
+    shuffledAnswers.sort((a, b) => Math.random() - 0.5 );
 
     return (
     <div id="quiz">
